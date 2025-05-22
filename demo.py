8fe2320286cc58ba6014fd6f3f0198020cd10ebc
Naga Aravind Kundeti <nfk5351@psu.edu>
2025-05-22 14:37:44 -0400

    second commit commit

---
+++ demo.py
@@ -0,0 +1,8 @@
+#Sample code
+print("\nHello Word!\n")
+for i in range(0,10):
+    for j in range(0,i+1):
+        print("* ", end="")
+    print(" ")
+print("\nEnd of the star pattern")
+print("Done by Naga Aravind Kundeti")
